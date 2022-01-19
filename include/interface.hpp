bfc124311bc3400fe803e1d9b41bddbf54cd6e98
Anchel17 <80929766+Anchel17@users.noreply.github.com>
2022-01-19 20:03:18 -0300

    Recebendo a Entrada

--- include/interface.hpp
+++ include/interface.hpp
@@ -7,7 +7,23 @@
  * imprimir a saída
  */
 class Interface{
+    private:
 
+        std::string interf_entrada;                  //<!Entrada recebida do usuário
+
+        //Process exit;                             //<!Objeto para chamar o vector de termos possíveis
+
+    public:
+        /**
+        *   Método para receber a entrada
+        */
+        void recebeEntrada();
+
+        /**
+        *   Método para retornar a entrada
+        *   @return string contendo a entrada digitada
+        */
+        std::string getEntrada();
 };
 
 #endif
