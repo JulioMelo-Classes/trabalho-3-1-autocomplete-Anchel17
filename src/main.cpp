ec5039a494c9eb09f5e783b92ad6952826bf06fe
Anchel17 <80929766+Anchel17@users.noreply.github.com>
2022-01-19 19:57:16 -0300

    Recebendo a entrada

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,7 @@
 #include <fstream>
 
 #include "../include/database.hpp"
+#include "../include/interface.hpp"
 
 using namespace std;
 
@@ -9,5 +10,11 @@ int main(int argc, char* argv[]){
 
     Database dado(argv[1]);
 
+    Interface menu;
+    string entrada = "";
+
+    while(true){
+        menu.recebeEntrada();
+    }
     return 0;
 }
