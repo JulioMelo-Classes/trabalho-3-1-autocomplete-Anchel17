c7c9b16b8338ed258af51faafa9456ac319d1965
Anchel17 <80929766+Anchel17@users.noreply.github.com>
2022-01-19 23:36:04 -0300

    Update interface.cpp

--- src/interface.cpp
+++ src/interface.cpp
@@ -7,7 +7,8 @@ using namespace std;
 
 void Interface::recebeEntrada(){
     cout<<"Digite uma palavra e tecle ENTER ou <ctrl + d> para encerrar o programa"<<endl;
-    getline(cin, interf_entrada); 
+    getline(cin, interf_entrada);
+
 
     if(cin.eof()){
         cout<<"Saindo..."<<endl;
