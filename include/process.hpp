1bd1cdc795fe4df7a1920666ffe1ec5ae0bf72a0
Anchel17 <80929766+Anchel17@users.noreply.github.com>
2022-01-19 23:37:05 -0300

    Update process.hpp

--- include/process.hpp
+++ include/process.hpp
@@ -1,13 +1,33 @@
 #ifndef PROCESS_HPP
 #define PROCESS_HPP
 
+#include <vector>
+
 /**
  * Classe responsável por processar o resultado da busca
  * do autocomplete e devolver os termos já ordenados para
  * serem exibidos ao usuário
  */
 class Process{
+    private:
+        std::vector<std::pair<unsigned int, std::string>>proc_result;             //<!O Vector resultante da busca
 
+    public:
+
+        /**
+        *   Método que vai armazenar no vector caso sejam encontrados termos
+        *   compatíveis com a entrada
+        *   @param string contendo o termo a ser armazenado
+        *   @param int contendo a ocorrência do termo
+        */
+        void achouArmazena(unsigned int &ocorr, std::string &termo);
+
+        /**
+        *   Método para buscar um termo no vector de dados
+        *   @param vector com referências aos dados
+        *   @param string termo a ser procurado
+        */
+        void busca(std::vector<std::pair<unsigned int, std::string>> dados, std::string termo);
 };
 
 #endif
