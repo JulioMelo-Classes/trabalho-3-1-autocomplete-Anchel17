b703f8fc84d364727812cf5f7df5324f059c11d2
Anchel17 <80929766+Anchel17@users.noreply.github.com>
2022-01-22 16:33:00 -0300

    Update interface.hpp

--- include/interface.hpp
+++ include/interface.hpp
@@ -1,6 +1,7 @@
 #ifndef INTERFACE_HPP
 #define INTERFACE_HPP
 
+#include "../include/process.hpp"
 /**
  * Classe responsável pela interação com o usuário
  * Vai imprimir as mensagens, receber a entrada e 
@@ -11,13 +12,18 @@ class Interface{
 
         std::string interf_entrada;                  //<!Entrada recebida do usuário
 
-        //Process exit;                             //<!Objeto para chamar o vector de termos possíveis
+        Process interf_imp;                             //<!Objeto para chamar o vector de termos possíveis
 
     public:
         /**
         *   Método para receber a entrada
         */
         void recebeEntrada();
+        
+        /**
+        *   Método para imprimir o vetor resultado
+        */
+        void imprimeResult();
 
         /**
         *   Método para retornar a entrada
