e863c66c8830e05a04f1af4050c935375d978cc8
Anchel17 <80929766+Anchel17@users.noreply.github.com>
2022-01-22 12:42:35 -0300

    Busca ok
    Tratamento de string está ok

--- src/process.cpp
+++ src/process.cpp
@@ -16,11 +16,6 @@ void Process::teste(){
 
 void Process::achouArmazena(unsigned int &ocorr, string &termo){
     this -> proc_result.push_back({ocorr, termo});
-
-    /*for(auto it : proc_result){
-        cout<<it.first<<" ";
-        cout<<it.second<<endl;
-    }*/
 }
 
 void Process::busca(vector<pair<unsigned int, string>> dados, string termo){
@@ -28,10 +23,9 @@ void Process::busca(vector<pair<unsigned int, string>> dados, string termo){
     //função lambda para deixar tudo com lowercase
     transform(termo.begin(), termo.end(), termo.begin(),
     [](char c){return tolower(c);});
-
         for(int i = 0; i < dados.size(); i++){
-            if(dados[i].second.find_first_of("\t") != string::npos){
-                size_t start = dados[i].second.find_first_of(" \t");    ///tirar tabulação
+            while(dados[i].second.find_first_of("\t") != string::npos){
+                size_t start = dados[i].second.find_first_of("\t");    ///tirar tabulação
                 dados[i].second.erase(dados[i].second.begin() + start);
             }
             
@@ -40,16 +34,6 @@ void Process::busca(vector<pair<unsigned int, string>> dados, string termo){
                 achouArmazena(dados[i].first, dados[i].second);
             }
     }
-    /*
-    int q = dados.size()-1;
-    int p = 0;
-    while(q - p + 1 > 0){
-        int i = (p + q)/2;
-        if(dados[i].second.find(termo) != string::npos){
-            achouArmazena(dados[i].first, dados[i].second);
-        }
-        if(dados[i].first)
-    }*/
 }
 
 void Process::limpa(){
