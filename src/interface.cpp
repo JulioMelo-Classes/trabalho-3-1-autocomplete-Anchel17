6f97e434283b3d399f1f12c5bc4a9894d7255863
Anchel17 <80929766+Anchel17@users.noreply.github.com>
2022-01-25 13:29:13 -0300

    Update interface.cpp

--- src/interface.cpp
+++ src/interface.cpp
@@ -15,10 +15,20 @@ void Interface::recebeEntrada(){
         cout<<"\n>>> Saindo..."<<endl;
         exit(0);
     }
+
+    cout<<">>> Resultados para \""<<interf_entrada<<"\":"<<endl;
 }
 
-void Interface::imprimeResult(){
-    cout<<">>> Resultados para \""<<interf_entrada<<"\":"<<endl;
+void Interface::imprimeResult(vector<pair<unsigned int, string>>& interf_Dados){
+    if(interf_Dados.empty()){
+        cout<<"Sem resultados..."<<endl;
+    }
+    
+    for(int i = 0; i < interf_Dados.size(); i++){
+        cout<<interf_Dados[i].first<<" ";
+        cout<<interf_Dados[i].second<<endl;
+    }
+    cout<<"\n\n\n";
 }
 
 string Interface::getEntrada(){
